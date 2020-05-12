1ccaf7ff2aa9c917098383e29396e7daaaf7329e
jengwa <48163855+Jengwa@users.noreply.github.com>
2020-05-12 05:55:55 +0200

    Add files via upload
    adding files for routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 //import HomePage from './pages/Home-page/HomePage';
 import Directory from './components/directory/Directory';
+import { Route, Switch } from 'react-router-dom';
 
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    
-      <Directory/>
-    
+    <div>
+      <Switch>
+        <Route exact path='/' component={Directory} />
+        <Route  path='/hats' component={HatsPage} />
+      </Switch>
+
+    </div>
+      
   );
 }
 
