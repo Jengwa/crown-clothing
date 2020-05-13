cedf15b6ac3b3907af029a5e3474a6b09ae48dc1
jengwa <48163855+Jengwa@users.noreply.github.com>
2020-05-13 09:18:41 +0200

    Add files via upload
    uploading my collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css';
-//import HomePage from './pages/Home-page/HomePage';
-import Directory from './components/directory/Directory';
+import HomePage from './pages/Home-page/HomePage';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/ShopPage';
 
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  )
-}
+
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Directory} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
