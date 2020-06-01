69ab65dc8c88c697e198cc4f0c56b3417f54d3e6
jengwa <48163855+Jengwa@users.noreply.github.com>
2020-06-01 20:47:42 +0200

    Add files via upload
    adding redux state to the cart icon and cart dropdown

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../../firebase/FirebaseUtils'
 
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
 
 import './Header.scss'
 
-const Header = ({ currentUser }) =>{
+
+const Header = ({ currentUser , hidden}) =>{
 	return (
 		<div className='header'>
 			<Link to='/' className='logo-container'>
@@ -27,13 +29,18 @@ const Header = ({ currentUser }) =>{
 					:
 					<Link to='/signin' className='option'> SIGN IN</Link>
 				}
+				<CartIcon />
 			</div>
+			{ 
+				hidden ? null : <CartDropdown/>
+			}
 		</div>
 	)
 }
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden } }) => ({
+	currentUser,
+	hidden
 })
 
 export default connect(mapStateToProps)(Header);
